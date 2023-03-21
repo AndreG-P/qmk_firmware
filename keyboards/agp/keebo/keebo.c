b82e7e328f156e2a0c6c0c29620f80ae93b3ee5a
AndreG-P <andre.greiner-petter@t-online.de>
2023-03-21 13:17:29 +0900

    Switch back to standard ISSI driver

--- keyboards/agp/keebo/keebo.c
+++ keyboards/agp/keebo/keebo.c
@@ -1,7 +1,7 @@
 #include "keebo.h"
 #include "print.h"
-#include "i2c_master.h"
-#include "drivers/led/issi/is31fl3741.h"
+// #include "i2c_master.h"
+// #include "drivers/led/issi/is31fl3741.h"
 // #include "gpio.h"
 
 char *status;
@@ -35,7 +35,7 @@ led_config_t g_led_config = { {
   4, 4, 4, 4
 } };
 
-bool issiInit = false;
+// bool issiInit = false;
 
 // static void initIssi(void);
 
@@ -72,67 +72,67 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 //     IS31FL3741_update_led_control_registers(DRIVER_ADDR_1, 0);
 // }
 
-i2c_status_t ret;
+// i2c_status_t ret;
 
 
 // Custom Driver
-static void init(void) {
-    setPinOutput( A5 );
+// static void init(void) {
+//     setPinOutput( A5 );
 
-    i2c_init();
+//     i2c_init();
 
-    print("right after i2c init printing... hope that works?");
+//     print("right after i2c init printing... hope that works?");
 
-    uint8_t IS31FL3741_REG_ID = 0xFC;
-    uint16_t delay = 0xFFU;
-    // if ( i2c_readReg( 0x60, IS31FL3741_REG_ID, &id, 1U, delay ) == 0 ) {
-    //     status = "Success\n";
-    // } else {
-    //     status = "ReadReg error\n";
-    // }
-    if ( i2c_transmit( DRIVER_ADDR_1 << 1, &IS31FL3741_REG_ID, 0x00000001U, delay ) == 0 ) {
-        if ( i2c_receive( DRIVER_ADDR_1 << 1, &id, 0x00000001U, delay ) == 0 ) {
-            if ( id == DRIVER_ADDR_1 << 1 ) {
-                status = "Success\n";
-                writePinHigh( A5 );
-            }
-        } else {
-            status = "Unable to receive after successful initial transmit\n";
-        }
-    } else {
-        status = "Error during first transmit of reg address\n";
-    }
+//     uint8_t IS31FL3741_REG_ID = 0xFC;
+//     uint16_t delay = 0xFFU;
+//     // if ( i2c_readReg( 0x60, IS31FL3741_REG_ID, &id, 1U, delay ) == 0 ) {
+//     //     status = "Success\n";
+//     // } else {
+//     //     status = "ReadReg error\n";
+//     // }
+//     if ( i2c_transmit( DRIVER_ADDR_1 << 1, &IS31FL3741_REG_ID, 0x00000001U, delay ) == 0 ) {
+//         if ( i2c_receive( DRIVER_ADDR_1 << 1, &id, 0x00000001U, delay ) == 0 ) {
+//             if ( id == DRIVER_ADDR_1 << 1 ) {
+//                 status = "Success\n";
+//                 writePinHigh( A5 );
+//             }
+//         } else {
+//             status = "Unable to receive after successful initial transmit\n";
+//         }
+//     } else {
+//         status = "Error during first transmit of reg address\n";
+//     }
 
-    IS31FL3741_init(DRIVER_ADDR_1);
+//     IS31FL3741_init(DRIVER_ADDR_1);
 
-    // sets scaling registers to 0xFF
-    for ( int i = 0; i < RGB_MATRIX_LED_COUNT; i++ )
-        IS31FL3741_set_led_control_register(i, true, true, true);
-    IS31FL3741_update_led_control_registers(DRIVER_ADDR_1, 0);
+//     // sets scaling registers to 0xFF
+//     for ( int i = 0; i < RGB_MATRIX_LED_COUNT; i++ )
+//         IS31FL3741_set_led_control_register(i, true, true, true);
+//     IS31FL3741_update_led_control_registers(DRIVER_ADDR_1, 0);
 
-    print("Init ISSI finished\n");
-}
+//     print("Init ISSI finished\n");
+// }
 
-static void flush(void) {
-    IS31FL3741_update_pwm_buffers(DRIVER_ADDR_1, 0);
-    // print("Flush ISSI\n");
-    wait_ms(10);
-}
+// static void flush(void) {
+//     IS31FL3741_update_pwm_buffers(DRIVER_ADDR_1, 0);
+//     // print("Flush ISSI\n");
+//     wait_ms(10);
+// }
 
-static void set_color(int index, uint8_t red, uint8_t green, uint8_t blue) {
-    IS31FL3741_set_color(index, red, green, blue);
-    wait_ms(10);
-}
+// static void set_color(int index, uint8_t red, uint8_t green, uint8_t blue) {
+//     IS31FL3741_set_color(index, red, green, blue);
+//     wait_ms(10);
+// }
 
-static void set_color_all(uint8_t red, uint8_t green, uint8_t blue) {
-    IS31FL3741_set_color_all( red, green, blue );
-    // uprintf("Set all colors on ISSI [0x%02X, 0x%02X, 0x%02X]\n", red, green, blue);
-    wait_ms(10);
-}
+// static void set_color_all(uint8_t red, uint8_t green, uint8_t blue) {
+//     IS31FL3741_set_color_all( red, green, blue );
+//     // uprintf("Set all colors on ISSI [0x%02X, 0x%02X, 0x%02X]\n", red, green, blue);
+//     wait_ms(10);
+// }
 
-const rgb_matrix_driver_t rgb_matrix_driver = {
-    .init = init,
-    .flush = flush,
-    .set_color = set_color,
-    .set_color_all = set_color_all
-};
+// const rgb_matrix_driver_t rgb_matrix_driver = {
+//     .init = init,
+//     .flush = flush,
+//     .set_color = set_color,
+//     .set_color_all = set_color_all
+// };
