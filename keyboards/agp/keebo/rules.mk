509cbbccff55205f9c863d8704d9939a382694b5
AndreG-P <andre.greiner-petter@t-online.de>
2023-05-18 18:37:38 +0900

    Trying to fix clock for entering bootloader without success

--- keyboards/agp/keebo/rules.mk
+++ keyboards/agp/keebo/rules.mk
@@ -1,6 +1,9 @@
 # MIN EXAMPLE
 # AKB/VERO
 # rules.mk, mcuconf.h, info.json, config.h, keymaps/default
+MCU = STM32F411
+BOARD = GENERIC_STM32_F411XE
+BOOTLOADER = stm32-dfu
 
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = IS31FL3741
